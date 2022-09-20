87f5ed15a5a059cd3eaa19094c1666cf86fa453e
Yash Lad <yashlad727@gmail.com>
2022-09-20 11:47:35 +0530

    wallet balance function added

--- AirdropProject1/index.js
+++ AirdropProject1/index.js
@@ -11,5 +11,26 @@ const Wallet = new Keypair()
 const publicKey = new PublicKey(Wallet._keypair.publicKey)
 const secretKey = Wallet._keypair.secretKey
 
-console.log(publicKey)
-console.log(secretKey);
+// console.log(publicKey)
+// console.log(secretKey);
+
+// Web3,js allows us to view our balance using the getbalance clause in Connection method
+// devnet -> replica of mainnet of solana 
+// cluster api gives the url of devnet network
+
+const getWalletBalance = async() => {
+    try{
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey);
+
+        console.log(`Wallet Balance is ${walletBalance}`)
+    }catch(err) {
+        console.error(err)
+    }
+}
+
+const main = async() => {
+    await getWalletBalance()
+}
+
+main()
